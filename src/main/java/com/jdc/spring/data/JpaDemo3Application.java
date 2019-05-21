f4b594fcb28860e4546979da12fdb5f894f62b43
User <User@DESKTOP-6N8DB9V>
2019-05-21 14:00:56 +0630

    Initial Commit

---
+++ src/main/java/com/jdc/spring/data/JpaDemo3Application.java
@@ -0,0 +1,13 @@
+package com.jdc.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDemo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDemo3Application.class, args);
+	}
+
+}
